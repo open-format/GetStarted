915d28b1e4f66ea51c99acff62eec504af151508
GenMusic <98533777+GenMusic@users.noreply.github.com>
2023-04-04 09:33:53 +0100

    Added dummy button and front-end action logic

--- classes/Action.ts
+++ classes/Action.ts
@@ -1,23 +1,29 @@
-// import RewardService from "../services/RewardService";
+import actions from "../actions.json";
 
-// export default class Action {
-//   action: ActionType;
+class Action {
+  action: ActionType;
 
-//   constructor(id: string) {
-//     const action = actions[id];
-//     if (action) {
-//       this.action = action;
-//    } else {
-//   throw new Error("Action not found");
-//  }
-//  }
+  constructor(id: string) {
+    const action = actions.find((action: ActionType) => action.id === id);
+    if (action) {
+      this.action = action;
+    } else {
+      throw new Error("Action not found");
+    }
+  }
 
-//   async trigger(address: string): Promise<any> {
-//     const { receiver, token, xp } = this.action;
-//     return RewardService.triggerReward(address, token, xp);
-//   }
+  async trigger(address?: string): Promise<any> {
+    const { token, xp } = this.action;
+    console.log(
+      `Triggering action for address: ${address}, token: ${token}, xp: ${xp}`
+    );
+    // replace console.log with actual logic to trigger action
+    return Promise.resolve();
+  }
 
-//   get(): ActionType {
-//     return this.action;
-//   }
-// }
+  get(): ActionType {
+    return this.action;
+  }
+}
+
+export default Action;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
+import React, { useCallback } from "react";
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Action from "../classes/Action";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleConnect = useCallback(async () => {
+    const connectAction = new Action("connect");
+    try {
+      await connectAction.trigger();
+      alert(`Action completed: ${connectAction.get().description}`);
+    } catch (error) {
+      console.error("Error in connect action:", error);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -13,7 +25,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>Home</main>
+      <main className={styles.main}>
+        <button onClick={handleConnect}>Connect</button>
+      </main>
     </>
   );
 }
