1926cc1f0df5ee7c108dd75fe89f54ee2126d47c
Andy <andy@openformat.tech>
2023-04-06 10:15:30 +0100

    reward system and service

--- utils/services/RewardService.ts
+++ utils/services/RewardService.ts
@@ -1,7 +1,48 @@
+import {
+  getActionsByUserAndRequirements,
+  getMissionsByUserAndRequirements,
+} from "@/queries";
+import { RewardParams } from "@/types";
+import { OpenFormatSDK } from "@openformat/sdk";
 import axios from "axios";
 
 class RewardService {
-  static async triggerReward(data: RewardParams) {
+  sdk: OpenFormatSDK;
+  constructor(sdk: OpenFormatSDK) {
+    this.sdk = sdk;
+  }
+
+  async getUserCompletedActions(address: string): Promise<string[]> {
+    const response = await this.sdk.subgraph.rawRequest(
+      getActionsByUserAndRequirements,
+      {
+        user: address.toLowerCase(),
+        app: this.sdk.appId.toLowerCase(),
+      }
+    );
+
+    const actionIds = response.actions.map(
+      (action) => action.type_id
+    );
+    return actionIds;
+  }
+
+  async getUserCompletedMissions(address: string): Promise<string[]> {
+    const response = await this.sdk.subgraph.rawRequest(
+      getMissionsByUserAndRequirements,
+      {
+        user: address.toLowerCase(),
+        app: this.sdk.appId.toLowerCase(),
+      }
+    );
+
+    const missionIds = response.missions.map(
+      (mission) => mission.type_id
+    );
+    return missionIds;
+  }
+
+  async trigger(data: RewardParams): Promise<void> {
     try {
       const res = await axios.post("api/reward", data);
       return res.data;
