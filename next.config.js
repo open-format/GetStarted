84896a5583f36274179253b3855386b9fa4f5480
GenMusic <98533777+GenMusic@users.noreply.github.com>
2023-04-11 15:17:44 +0100

    next config update, add supabase env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+  },
 };
 
 module.exports = nextConfig;
