efa90235d21383c35c197a647bdbdf74b78d4392
GenMusic <98533777+GenMusic@users.noreply.github.com>
2023-04-12 13:15:47 +0100

    Remove API logic, handling using serversideprops in admin

--- pages/api/signInWithMagicLink.ts
+++ pages/api/signInWithMagicLink.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import Cookies from "cookies";
 import { supabase } from "../../utils/supabaseClient";
 
 export default async function handler(
@@ -13,33 +12,12 @@ export default async function handler(
 
     if (error) {
       return res.status(400).json({ error: error.message });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "Check your email for the magic link!" });
     }
-
-    // Get the user associated with the email
-    const { data: users, error: userError } = await supabase
-      .from("users")
-      .select("id")
-      .eq("email", email);
-
-    if (userError || !users || users.length === 0) {
-      return res.status(500).json({ error: "Error fetching user." });
-    }
-
-    const userId = users[0].id;
-
-    // Set the user id in an HTTP-only cookie
-    const cookies = new Cookies(req, res);
-    cookies.set("userId", userId, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      path: "/",
-    });
-
-    return res
-      .status(200)
-      .json({ message: "Check your email for the magic link!" });
-  } catch (error) {
-    return res.status(500).json({ error: "Error processing request." });
+  } catch (error: any) {
+    return res.status(500).json({ error: (error as Error).message });
   }
 }
