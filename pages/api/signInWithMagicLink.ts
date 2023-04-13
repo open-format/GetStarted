7a38d662e4872ba12d52ec34a4253fef0a5a06bb
GenMusic <98533777+GenMusic@users.noreply.github.com>
2023-04-13 13:52:41 +0100

    add url forwarding

--- pages/api/signInWithMagicLink.ts
+++ pages/api/signInWithMagicLink.ts
@@ -7,8 +7,25 @@ export default async function handler(
 ) {
   const { email } = req.body;
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to including trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   try {
-    const { error } = await supabase.auth.signInWithOtp({ email });
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: getURL(),
+      },
+    });
 
     if (error) {
       return res.status(400).json({ error: error.message });
