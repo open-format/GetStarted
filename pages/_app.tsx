3b45ac37b17b7f2958197db6be1982bd9aa85806
Andy <andy@openformat.tech>
2023-04-14 11:10:09 +0100

    add prettier config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
+import { Chains, OpenFormatProvider } from "@openformat/react";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
-import { OpenFormatProvider } from "@openformat/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <OpenFormatProvider
         config={{
-          network: process.env.NEXT_PUBLIC_NETWORK,
+          networks: [Chains.polygonMumbai],
           appId: process.env.NEXT_PUBLIC_APP_ID,
         }}
       >
