b5549d011de40966d34335907b48f59de0d7dca2
GenMusic <98533777+3rdBlock@users.noreply.github.com>
2023-04-14 12:56:59 +0100

    Update profile to display XP and badges

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,34 +1,42 @@
 import React from "react";
 import Head from "next/head";
-import { useRawRequest } from "@openformat/react";
+import { useRawRequest, useWallet } from "@openformat/react";
 import { gql } from "graphql-request";
-import { useWallet } from "@openformat/react";
 
 const Profile: React.FC = () => {
-  const { address } = useWallet();
+  const { address = null } = useWallet();
 
-  const { data } = useRawRequest({
-    query: gql`
-      query MyQuery($userId: String!) {
-        actions(where: { user: $userId }) {
-          type_id
+  const { actions = [], missions = [] } =
+    useRawRequest({
+      query: gql`
+        query MyQuery($userId: String!, $appId: String!) {
+          actions(where: { user: $userId, app: $appId }) {
+            type_id
+            amount
+          }
+          missions(where: { user: $userId, app: $appId }) {
+            type_id
+          }
         }
-        missions(where: { user: $userId }) {
-          type_id
-        }
-      }
-    `,
-    variables: { userId: address?.toLocaleLowerCase() },
-  });
+      `,
+      variables: {
+        userId: address?.toLocaleLowerCase(),
+        appId: process.env.NEXT_PUBLIC_APP_ID,
+      },
+    }).data || {};
 
-  const countActionTypes = (actions) => {
-    return actions.reduce((acc, action) => {
-      acc[action.type_id] = (acc[action.type_id] || 0) + 1;
-      return acc;
-    }, {});
+  const calculateActionCountsAndTokens = (actions) => {
+    const actionTypes = {};
+    let totalTokens = 0;
+    actions.forEach((action) => {
+      actionTypes[action.type_id] = (actionTypes[action.type_id] || 0) + 1;
+      totalTokens += Number(action.amount);
+    });
+    return { actionTypes, totalTokens };
   };
 
-  const actionsCount = data?.actions ? countActionTypes(data.actions) : {};
+  const { actionTypes, totalTokens } = calculateActionCountsAndTokens(actions);
+  const missionsCompleted = missions.length;
 
   return (
     <>
@@ -38,10 +46,10 @@ const Profile: React.FC = () => {
       {address ? (
         <div>
           <h1>Profile</h1>
-          <h2>Wallet Address:{address.toLocaleLowerCase()}</h2>
+          <h2>Wallet Address: {`${address?.toLocaleLowerCase()}`}</h2>
           <h3>Actions Completed:</h3>
           <ul>
-            {Object.entries(actionsCount).map(([type_id, count]) => (
+            {Object.entries(actionTypes).map(([type_id, count]) => (
               <li key={type_id}>
                 {type_id}, Completed: {count}
               </li>
@@ -49,10 +57,12 @@ const Profile: React.FC = () => {
           </ul>
           <h3>Missions Completed:</h3>
           <ul>
-            {data?.missions?.map((mission, index) => (
+            {missions.map((mission, index) => (
               <li key={index}>{mission.type_id}</li>
             ))}
           </ul>
+          <h3>Tokens Earned: {totalTokens} xp tokens</h3>
+          <h3>Badges Earned: {missionsCompleted}</h3>
         </div>
       ) : (
         <div>
