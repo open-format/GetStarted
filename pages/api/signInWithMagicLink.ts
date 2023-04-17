ef50b08dfc0e843754b11ee986913f0b6f994209
GenMusic <98533777+GenMusic@users.noreply.github.com>
2023-04-17 13:33:56 +0100

    fix stuck toast when network takes too long

---
+++ pages/api/signInWithMagicLink.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../utils/supabaseClient";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email } = req.body;
+
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to including trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    // Add the /profile route
+    url = `${url}`;
+    return url;
+  };
+
+  try {
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: getURL(),
+      },
+    });
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "Check your email for the magic link!" });
+    }
+  } catch (error: any) {
+    return res.status(500).json({ error: (error as Error).message });
+  }
+}
