c28ce67c84d749b169a12ffc4dc96e0f5a3a3eb7
Andy <andy@openformat.tech>
2023-04-20 11:27:56 +0100

    add useMounted hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Header from "../components/Header";
 import { Chains, OpenFormatProvider } from "@openformat/react";
-import { Toaster } from "react-hot-toast";
+import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
+import { useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
+import { Header } from "../components";
 import { checkEnvVariables } from "../env-config";
+
+// This is due to Wagmi and SSR. We hope to fix this within the Open Format SDK soon.
+// https://github.com/wagmi-dev/wagmi/issues/542#issuecomment-1144178142
+export function useIsMounted() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  return mounted;
+}
 
 // Load the Roboto font with specified weight and subset
 const roboto = Roboto({
@@ -15,6 +24,8 @@ const roboto = Roboto({
 // Main App component for the Next.js application
 export default function App({ Component, pageProps }: AppProps) {
   checkEnvVariables(); // Check for missing or empty environment variables
+  const isMounted = useIsMounted();
+  if (!isMounted) return;
   return (
     // Apply the Roboto font to the main container
     <main className={roboto.className}>
