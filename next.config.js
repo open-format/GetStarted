a61c675b922be76969a8aa05bd98e398ef51e62e
GenMusic <98533777+3rdBlock@users.noreply.github.com>
2023-04-26 11:13:00 +0100

    remove sitemap

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.xml",
-        destination: "/api/sitemap.xml",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
